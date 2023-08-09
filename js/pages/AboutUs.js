13bee148764e3d5ab8c2b380c0646416ea7cdc96
PariaRF <paria.rezaeefard@gmail.com>
2023-08-09 23:00:10 +0330

    change image path

--- js/pages/AboutUs.js
+++ js/pages/AboutUs.js
@@ -1,7 +1,7 @@
 const AboutUs = () => {
     return `<div class="maintnance__container">
     <h1>درباره ما</h1>
-    <img src="assets/images/maintnance.png" alt="maintnance"/>
+    <img src="/pariarf/assets/images/maintnance.png" alt="maintnance"/>
     </div>`;
 };
 
--- js/pages/Representation.js
+++ js/pages/Representation.js
@@ -1,7 +1,7 @@
 const Representation = () => {
     return `<div class="maintnance__container">
     <h1>اعطای نمایندگی</h1>
-    <img src="assets/images/maintnance.png"/>
+    <img src="/pariarf/assets/images/maintnance.png"/>
     </div>`;
 };
 
